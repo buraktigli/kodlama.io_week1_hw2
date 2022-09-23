bf81b8b478d5c49461271b42235f4d4142326731
burak <43644224+buraktigli@users.noreply.github.com>
2022-09-23 11:55:44 +0300

    JavaDersi11_forLoops

---
+++ JavaDersi11_forLoops/Main.java
@@ -0,0 +1,25 @@
+package JavaDersi11_forLoops;
+
+public class Main {
+
+	public static void main(String[] args) {
+ 
+		System.out.println("1den 9a kadar sayilar");
+		for (int i = 1; i < 10; i ++) {
+			System.out.println(i);
+		}
+		System.out.println("1den 9a kadar cift sayilar");
+		
+		for (int i = 2; i < 10; i += 2) {
+			System.out.println(i);
+		}
+		System.out.println("1den 9a kadar tek sayilar");
+		
+		for (int i = 1; i < 10; i += 2) {
+			System.out.println(i);
+		}
+		
+		System.out.println("dongu bitti.");
+	}
+
+}
