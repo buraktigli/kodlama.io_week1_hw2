51981a34b97f2b4f55be40da36a5b8ed13e69db2
burak <43644224+buraktigli@users.noreply.github.com>
2022-09-25 23:13:51 +0300

    JavaDersi12_whileLoops

---
+++ JavaDersi12_whileLoops/Main.java
@@ -0,0 +1,31 @@
+package JavaDersi12_whileLoops;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		int i = 1;
+
+		// while loops
+		while (i < 10) {
+			System.out.println(i);
+			i++;
+
+		}
+		System.out.println("while loops finished.");
+	
+	
+	//do while loops
+	
+	int j=10;
+	do {
+		System.out.println(j);
+		j+=2;
+    }
+	
+	while(j<10);
+		
+	System.out.println("do while loops finished.");
+}
+}
+
