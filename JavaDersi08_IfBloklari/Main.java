807891f06389fcc57e732935580b26dfeb104242
burak <43644224+buraktigli@users.noreply.github.com>
2022-11-29 16:39:28 +0300

    Add files via upload

---
+++ JavaDersi08_IfBloklari/Main.java
@@ -0,0 +1,20 @@
+package JavaDersi8_IfBloklari;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		int sayi=29;
+		
+        if(sayi<20) {
+        	System.out.println("sayi 20den kucuktur.");
+        }
+        else if(sayi==20) {
+        	System.out.println("sayi 20ye esittir.");
+        }
+        else {
+        	System.out.println("sayi 20den buyuktur.");
+        }
+	}
+
+}
