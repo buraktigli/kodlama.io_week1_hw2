2b3cac72f61297a4a4f07a69739b802021bfb4cb
burak <43644224+buraktigli@users.noreply.github.com>
2022-11-29 16:38:22 +0300

    Add files via upload

---
+++ JavaDersi07_TemelVeriTipleri/Main.java
@@ -0,0 +1,24 @@
+package JavaDersi7_TemelVeriTipleri;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		double sayi= 21.5;
+		int sayi1 = -127;
+		
+		char karakter = 'A';
+		String sehir = "Ankara";
+		
+		boolean dogruMu = false;
+		
+		
+		System.out.println(sayi);
+		System.out.println(sayi1);
+		System.out.println(karakter);
+		System.out.println(sehir);
+		System.out.println(dogruMu);
+
+	}
+
+}
