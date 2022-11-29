37a25814b4459b69ad7d1d87b45d00ff566b8821
burak <43644224+buraktigli@users.noreply.github.com>
2022-11-29 16:38:09 +0300

    Add files via upload

---
+++ JavaDersi06_Degiskenler/Main.java
@@ -0,0 +1,14 @@
+package JavaDersi6_Degiskenler;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+      int ogrenciSayisi = 28;
+      String mesaj = "Ogrenci Sayisi: ";
+      
+      System.out.println(mesaj + ogrenciSayisi);
+
+	}
+
+}
